31562dd569be17dc701e19cfa2a73dc4a7169a92
Beknur <beknur.les06@gmail.com>
2025-10-18 18:40:10 +0500

    swagger

--- src/main/java/com/kz/zamanbankapi/controller/CardController.java
+++ src/main/java/com/kz/zamanbankapi/controller/CardController.java
@@ -3,6 +3,8 @@ package com.kz.zamanbankapi.controller;
 import com.kz.zamanbankapi.dto.CardCreationRequest;
 import com.kz.zamanbankapi.dto.CardDto;
 import com.kz.zamanbankapi.service.CardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
