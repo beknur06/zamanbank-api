aee85334b983ffbd8c09fd15978c2e54c92d5d23
Beknur <beknur.les06@gmail.com>
2025-10-18 20:12:52 +0500

    redundant line

--- src/main/java/com/kz/zamanbankapi/dto/CardCreationRequest.java
+++ src/main/java/com/kz/zamanbankapi/dto/CardCreationRequest.java
@@ -1,12 +1,8 @@
 package com.kz.zamanbankapi.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CardCreationRequest {
-    @NotNull(message = "User ID is required")
-    private Long userId;
-
     private String designPreferences;
 }
